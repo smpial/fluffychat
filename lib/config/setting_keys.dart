33e13165d8499208a6cb7ed77c7c841e8a64d61c
smpial <66345743+smpial@users.noreply.github.com>
2025-07-11 23:35:53 +0600

    Update setting_keys.dart

--- lib/config/setting_keys.dart
+++ lib/config/setting_keys.dart
@@ -46,7 +46,7 @@ enum AppSettings<T> {
   audioRecordingSamplingRate<int>('audioRecordingSamplingRate', 44100),
   pushNotificationsGatewayUrl<String>(
     'pushNotificationsGatewayUrl',
-    'https://push.fluffychat.im/_matrix/push/v1/notify',
+    'https://matrix-push.klucva.ru/_matrix/push/v1/notify',
   ),
   pushNotificationsPusherFormat<String>(
     'pushNotificationsPusherFormat',
