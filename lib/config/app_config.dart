e72b81fc6b5a1bf145aa66a7616fbcc40cf1afc8
smpial <66345743+smpial@users.noreply.github.com>
2026-02-25 12:41:18 +0600

    Update app_config.dart

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -12,52 +12,52 @@ abstract class AppConfig {
   static const bool enableRegistration = true;
   static const bool hideTypingUsernames = false;
 
-  static const String inviteLinkPrefix = 'https://chat.klucva.ru/#/';
-  static const String deepLinkPrefix = 'ru.klucva.fluffychat://chat/';
+  static const String inviteLinkPrefix = 'https://matrix.to/#/';
+  static const String deepLinkPrefix = 'im.fluffychat://chat/';
   static const String schemePrefix = 'matrix:';
   static const String pushNotificationsChannelId = 'fluffychat_push';
-  static const String pushNotificationsAppId = 'ru.klucva.fluffychat';
+  static const String pushNotificationsAppId = 'chat.fluffy.fluffychat';
   static const double borderRadius = 18.0;
   static const double spaceBorderRadius = 11.0;
   static const double columnWidth = 360.0;
 
-  static const String website = 'https://chat.klucva.ru';
+  static const String website = 'https://fluffy.chat';
   static const String enablePushTutorial =
-      'https://chat.klucva.ru/fluffychat/faq/#push_without_google_services';
+      'https://fluffy.chat/faq/#push_without_google_services';
   static const String encryptionTutorial =
-      'https://chat.klucva.ru/fluffychat/faq/#how_to_use_end_to_end_encryption';
+      'https://fluffy.chat/faq/#how_to_use_end_to_end_encryption';
   static const String startChatTutorial =
-      'https://chat.klucva.ru/fluffychat/faq/#how_do_i_find_other_users';
+      'https://fluffy.chat/faq/#how_do_i_find_other_users';
   static const String howDoIGetStickersTutorial =
-      'https://chat.klucva.ru/fluffychat/faq/#how_do_i_get_stickers';
+      'https://fluffy.chat/faq/#how_do_i_get_stickers';
   static const String appId = 'im.fluffychat.FluffyChat';
   static const String appOpenUrlScheme = 'chat.fluffy';
 
   static const String sourceCodeUrl =
-      'https://github.com/smpial/fluffychat';
+      'https://github.com/krille-chan/fluffychat';
   static const String supportUrl =
-      'https://github.com/smpial/fluffychat/issues';
-  static const String changelogUrl = 'https://chat.klucva.ru/fluffychat/en/changelog/';
-  static const String donationUrl = 'https://chat.klucva.ru';
+      'https://github.com/krille-chan/fluffychat/issues';
+  static const String changelogUrl = 'https://fluffy.chat/en/changelog/';
+  static const String donationUrl = 'https://ko-fi.com/krille';
 
   static const Set<String> defaultReactions = {'👍', '❤️', '😂', '😮', '😢'};
 
   static final Uri newIssueUrl = Uri(
     scheme: 'https',
     host: 'github.com',
-    path: '/smpial/fluffychat/issues/new',
+    path: '/krille-chan/fluffychat/issues/new',
   );
 
   static final Uri homeserverList = Uri(
     scheme: 'https',
     host: 'raw.githubusercontent.com',
-    path: 'smpial/fluffychat/refs/heads/main/recommended_homeservers.json',
+    path: 'krille-chan/fluffychat/refs/heads/main/recommended_homeservers.json',
   );
 
   static final Uri privacyUrl = Uri(
     scheme: 'https',
-    host: 'chat.klucva.ru',
-    path: '/fluffychat/en/privacy',
+    host: 'fluffy.chat',
+    path: '/en/privacy',
   );
 
   static const String mainIsolatePortName = 'main_isolate';
