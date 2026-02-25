82e3f055c2853503acab1d4df45a3221e761d02e
smpial <66345743+smpial@users.noreply.github.com>
2026-02-25 14:59:30 +0600

    Update check_homeserver.dart

--- lib/utils/sign_in_flows/check_homeserver.dart
+++ lib/utils/sign_in_flows/check_homeserver.dart
@@ -58,7 +58,7 @@ void connectToHomeserverFlow(
       final pathSegments = List.of(
         GoRouter.of(context).routeInformationProvider.value.uri.pathSegments,
       );
-      pathSegments.removeLast();
+      if(pathSegments.isNotEmpty && pathSegments.last != "home") pathSegments.removeLast();
       pathSegments.add('login');
       context.go('/${pathSegments.join('/')}', extra: client);
       setState(AsyncSnapshot.withData(ConnectionState.done, true));
